8b53af9a48c2f514b70ba849967a7669a4c28bc0
Wladelets <wladdelets@gmail.com>
2025-06-12 02:12:54 +0300

    Update telegram_weather_bot.py

--- telegram_weather_bot.py
+++ telegram_weather_bot.py
@@ -109,7 +109,7 @@ bot_app.add_error_handler(error_handler)
 @app.post(WEBHOOK_PATH)
 async def telegram_webhook(req: Request):
     data = await req.json()
-    print("UPDATE INCOMING:", data)
+
     await bot_app.update_queue.put(Update.de_json(data, bot_app.bot))
     return {"ok": True}
 
